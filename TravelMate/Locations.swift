11822a3315da533e1be715cc5d7d8b3a72b9b5e3
Olivia Dugger <oliviacdugger@gmail.com>
2019-06-26 16:34:36 -0500

    Day One

---
+++ TravelMate/Locations.swift
@@ -0,0 +1,14 @@
+//
+//  Locations.swift
+//  TravelMate
+//
+//  Created by Olivia Dugger on 6/26/19.
+//  Copyright © 2019 Olivia Dugger. All rights reserved.
+//
+
+import UIKit
+
+class city {
+    var name = " "
+    var important = false
+}
---
+++ TravelMate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TravelMate
+//
+//  Created by Olivia Dugger on 6/26/19.
+//  Copyright © 2019 Olivia Dugger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func chooseYourTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func destinationTapped(_ sender: UIButton) {
+    }
+    
+}
+
